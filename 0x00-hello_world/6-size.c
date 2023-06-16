ee25e0fc5382b600ab86060248be18e1a521e23a
AltUena9 <neemoangel09@gmail.com>
2023-06-15 17:40:21 -0700

    Print size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 		"Size of a long int: %d byte(s)\n", sizeof(long int)
 		"Size of a long long int: %d byte(s)\n", sizeof(long long int)
 		"Size of a float: %d byte(s)", sizeof(float)
-		)
-	return (0)
+		);
+	return (0);
 }
