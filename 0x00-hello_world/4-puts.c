e659d898eb64c76a1b80620185736dd760940daa
AltUena9 <neemoangel09@gmail.com>
2023-06-15 17:01:14 -0700

    Puts No printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: 0 if code success
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,\n")
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
