a99cda41502ad1d5bbf56387550eeb82b162d1a7
AltUena9 <neemoangel09@gmail.com>
2023-06-19 13:55:19 -0700

    Fucking numberz!

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ * Code for 0-9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
