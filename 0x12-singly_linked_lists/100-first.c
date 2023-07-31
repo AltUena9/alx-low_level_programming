e0e9ba779859f29eef8502582bcb9f30d8553854
AltUena9 <neemoangel09@gmail.com>
2023-07-31 06:25:04 -0700

    Story time

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * game - applies constructor attribute
+ */
+void game(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
