4cf856f56416824ebba146578d382e55e758ffc3
AltUena9 <neemoangel09@gmail.com>
2023-07-31 07:03:57 -0700

    Work

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
+void __attribute__((constructor)) game(void);
 /**
  * game - applies constructor attribute
  */
 void game(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
